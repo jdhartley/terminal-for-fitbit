615f2d6bb4f3964768305d879be331f60f65c5d3
JD Hartley <jdhartley@users.noreply.github.com>
2022-06-15 19:33:56 -0700

    Update battery line display when charging (#41)

--- app/datalines/battery.js
+++ app/datalines/battery.js
@@ -1,4 +1,5 @@
 import { battery } from 'power';
+import { clock } from 'clock';
 
 import Dataline from './Dataline';
 import { swapClass } from '../utils';
@@ -7,13 +8,16 @@ export default new Dataline({
     name: 'BATT',
     updateValue() {
         const level = Math.floor(battery.chargeLevel);
-        const hashCount = Math.floor(level / 10);
+        const isCharging = battery.charging;
+        const showChargingHash = isCharging && (new Date()).getSeconds() % 2 === 0;
+        const hashCount = Math.min(10, Math.floor(level / 10) + (showChargingHash ? 1 : 0));
         const dotCount = 10 - hashCount;
 
         const hashes = '#'.repeat(hashCount);
         const dots = '.'.repeat(dotCount);
         const spacer = level < 100 ? ' ' : '';
 
+        this.labelRef.text = `[${isCharging ? 'CHRG' : 'BATT'}]`;
         this.valueRef.text = `[${hashes}${dots}]${spacer}${level}%`;
 
         let color = 'green';
@@ -25,9 +29,23 @@ export default new Dataline({
         swapClass(this.valueRef.root, 'color', color);
     },
     start() {
+        this.onChargeHandler = () => {
+            if (battery.charging) {
+                clock.addEventListener('tick', this.updateValue);
+            } else {
+                clock.removeEventListener('tick', this.updateValue);
+            }
+        };
+
         battery.addEventListener('change', this.updateValue);
+        battery.addEventListener('change', this.onChargeHandler);
     },
     stop() {
         battery.removeEventListener('change', this.updateValue);
+
+        clock.removeEventListener('tick', this.updateValue);
+        battery.removeEventListener('change', this.onChargeHandler);
+
+        delete this.onChargeHandler;
     },
 });
